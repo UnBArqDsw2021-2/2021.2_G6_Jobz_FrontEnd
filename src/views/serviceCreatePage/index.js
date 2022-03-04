196a9cb225d2dc4d196aa64ce155f9fe789c787d
Luís Furtado <luiscesm1@gmail.com>
2022-03-04 16:10:53 -0300

    Criação base estrutura do formulário

--- src/views/serviceCreatePage/index.js
+++ src/views/serviceCreatePage/index.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../components/Button'
 import logo from '../../assets/logo.svg'
 import Input from '../../components/Input'
 import * as S from './styles'
 
-const ServiceCreatePage = () => (
-  <S.Container>
-    <header>
-      <img src={logo} alt="logo" />
-    </header>
+function ServiceCreatePage() {
+  const [form, setForm] = useState({
+    categroy: null,
+    name: null,
+    description: null,
+  });
 
-    <form>
-      <Input placeholder="CATEGORIA" type="text" required />
-      <Input placeholder="TÍTULO" type="text" required />
-      <Input placeholder="DESCRIÇÃO" type="textarea" required />
-      <br></br>
-      <Button type="submit" title={'CADASTRAR SERVIÇO'} />
-    </form>
-  </S.Container>
-)
+  return (
+    <S.Container>
+      <header>
+        <img src={logo} alt="logo" />
+      </header>
 
-export default ServiceCreatePage
+      <input></input>
+  
+      <form>
+        <Input placeholder="CATEGORIA" type="text" required />
+        <Input placeholder="TÍTULO" type="text" required />
+        <Input placeholder="DESCRIÇÃO" type="textarea" required />
+        <br></br>
+        <Button type="submit" title={'CADASTRAR SERVIÇO'} />
+      </form>
+    </S.Container>
+  )
+}
+
+export default ServiceCreatePage;
