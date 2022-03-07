848eb2d36d3923837e7d9ae781ebce96778026d5
Peh099 <70337717+Peh099@users.noreply.github.com>
2022-03-07 16:49:34 -0300

    Adicao conteudo pagina service
     Co-authored-by: Rodrigo Balbino <rodrigo080599@hotmail.com>

--- src/views/ServicesPage/index.js
+++ src/views/ServicesPage/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import * as S from './styles'
 import logo from '../../assets/LogoBlack.svg'
 import procura from '../../assets/procura.svg'
-import background from '../../assets/servico.svg'
+import background from '../../assets/service.png'
+import ti from '../../assets/TI.png'
+import jardinagem from '../../assets/Jardinagem.png'
+import encanador from '../../assets/Encanador.png'
+import logowhite from '../../assets/LogoWhite.svg'
 
 const ServicesPage = () => (
   <S.Container>
@@ -33,20 +37,53 @@ const ServicesPage = () => (
 
     <section
       class="section"
-      id="busca"
+      id="search"
       style={{ backgroundImage: `url(${background})` }}
     >
-      <div class="searchBar">
-        <p>
-          Encontre o serviço<br></br>certo para você
-        </p>
-        <div id="divBusca2">
-          <img src={procura} id="btnBusca" alt="procura" />
-          <input
-            type="text"
-            id="txtBusca1"
-            placeholder="Busque o que precisar"
-          />
+      <div class="container grid">
+        <div class="searchBar">
+          <p class="encontre">
+            Encontre o serviço<br></br>certo para você
+          </p>
+          <div id="divBusca2">
+            <img src={procura} id="btnBusca" alt="procura" />
+            <input
+              type="text"
+              id="txtBusca1"
+              placeholder="Busque o que precisar"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section" id="services">
+      <div class="container grid">
+        <div class="text">
+          <p>Serviços Populares</p>
+        </div>
+        <div class="box">
+          <img src={ti} id="ti" alt="imagem tecnico em ti" />
+          <br></br>
+          <span>Assistência em TI</span>
+        </div>
+        <div class="box">
+          <img src={encanador} id="encanador" alt="imagem encanador" />
+          <br></br>
+          <span> Encanadores</span>
+        </div>
+        <div class="box">
+          <img src={jardinagem} id="jardinagem" alt="imagem jardineiro" />
+          <br></br>
+          <span> Jardineiros</span>
+        </div>
+      </div>
+    </section>
+    <section class="section" id="footer">
+      <div class="container grid">
+        <img src={logowhite} id="logowhite" alt="logo branca" />
+        <div class="contact">
+          <p>+1 (2345) 678-90-12</p>
+          <p>support@personal.com</p>
         </div>
       </div>
     </section>
--- src/views/ServicesPage/styles.js
+++ src/views/ServicesPage/styles.js
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   #header {
     display: flex;
-    border-bottom: 1px solid #e4e4e4;
-    margin-bottom: 2rem;
+    border-bottom: 2px solid #2f7b77;
     width: 100%;
     position: fixed;
     top: 0;
@@ -14,8 +13,8 @@ export const Container = styled.div`
   }
 
   nav {
-    justify-content: space-around;
     height: 5.6rem;
+    justify-content: space-around;
     display: flex;
     align-items: center;
     width: 100%;
@@ -57,7 +56,7 @@ export const Container = styled.div`
     margin-left: -12rem;
     margin-top: 6rem;
     left: 50%;
-    top: 148px;
+    top: 11rem;
     background-color: #f5f5f5;
     height: 3.31rem;
     border: 1px solid #dadada;
@@ -82,6 +81,7 @@ export const Container = styled.div`
     text-indent: 1.5rem;
   }
   #txtBusca1 {
+    border-radius: 1.625rem;
     float: right;
     background-color: transparent;
     font-family: Montserrat;
@@ -96,21 +96,21 @@ export const Container = styled.div`
     text-indent: 1.5rem;
   }
 
-  #busca {
+  #search {
     position: absolute;
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center center;
     width: 100%;
-    height: 20rem;
+    height: 28rem;
     left: 0rem;
-    top: 5.8rem;
+    top: 5.2rem;
     p {
       position: absolute;
       margin-left: -6.8rem;
       margin-top: -3.5rem;
       left: 50%;
-      top: 148px;
+      top: 12rem;
 
       font-family: Montserrat;
       font-style: normal;
@@ -123,5 +123,81 @@ export const Container = styled.div`
 
       color: #f9f9f9;
     }
+  }
+  #services {
+    display: flex;
+    background-color: #fff;
+    margin-top: 33.2rem;
+    height: 33rem;
+  }
+  #services div.text {
+    margin-top: 2.25rem;
+  }
+  #services .text p {
+    position: absolute;
+    width: 18.25rem;
+    height: 2rem;
+
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 1.8rem;
+    text-align: center;
+
+    color: #2f7b77;
+  }
+
+  #services .photo {
+    position: absolute;
+    width: 43.75rem;
+    height: 18.75rem;
+    left: 8.75rem;
+    top: 26.5rem;
+  }
+  #services div.box {
+    text-align: center;
+    display: inline-block;
+    margin-left: 3.125rem;
+    margin-top: 6.25rem;
+  }
+  #services span {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.125rem;
+    color: #000000;
+  }
+  #footer {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    width: 100%;
+    height: 30rem;
+    left: 0rem;
+    top: 5.8rem;
+    display: flex;
+    background-color: #0a142f;
+  }
+  position: absolute;
+  left: 0rem;
+  right: 0rem;
+  #footer img {
+    margin-top: 7.3rem;
+  }
+  #footer div.contact {
+    margin-top: 1.5rem;
+  }
+  #footer p {
+    font-family: TT Commons;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 32px;
+    /* identical to box height, or 160% */
+
+    /* White */
+
+    color: #ffffff;
   }
 `
