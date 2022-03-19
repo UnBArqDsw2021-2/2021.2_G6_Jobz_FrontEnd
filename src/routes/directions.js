81163c233bb0f2f9096f52de5d92e5b628f03d97
Rodrigo Balbino <rodrigo080599@hotmail.com>
2022-03-19 12:22:36 -0300

    especificando branch

--- src/routes/directions.js
+++ src/routes/directions.js
@@ -1,10 +1,7 @@
 import UserRegistrationPage from '../views/userRegistrationPage/index'
 import CollaboratorRegistrationPage from '../views/collaboratorRegistrationPage/index'
-import ServiceCreatePage from '../views/serviceCreatePage/index'
 import HomePage from '../views/HomePage/index'
 import LoginPage from '../views/loginPage/index'
-import UserProfile from '../views/userProfile/index'
-import Security from '../views/security/index'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function directions() {
@@ -12,18 +9,9 @@ function directions() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route
-          path="/userRegistrationPage"
-          element={<UserRegistrationPage />}
-        />
-        <Route
-          path="/collaboratorRegistrationPage"
-          element={<CollaboratorRegistrationPage />}
-        />
-        <Route path="/serviceCreatePage" element={<ServiceCreatePage />} />
+        <Route path="/userRegistrationPage" element={<UserRegistrationPage />} />
+        <Route path="/collaboratorRegistrationPage" element={<CollaboratorRegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/userProfile" element={<UserProfile />} />
-        <Route path="/security" element={<Security />} />
       </Routes>
     </BrowserRouter>
   )
