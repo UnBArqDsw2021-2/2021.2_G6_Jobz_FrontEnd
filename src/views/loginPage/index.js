ceba415d7cc9e4b41e6812c43b2c6b71495d262a
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-04-14 15:49:51 -0300

    add icons on login page
    Co-authored-by: Rodrigo Balbino <rodrigo080599@hotmail.com>

--- src/views/loginPage/index.js
+++ src/views/loginPage/index.js
@@ -1,35 +1,34 @@
-import React, { useState, useContext } from 'react'
-import Button from '../../components/Button'
-import logo from '../../assets/logo.png'
-import Input from '../../components/Input'
-import * as S from './styles'
-import Navbar from '../../components/Navbar/navbar'
-import Footer from '../../components/Footer/footer'
-import { AuthContext } from "../../context/auth"
-
+import React, { useState, useContext, useEffect } from "react";
+import { MdOutlineMail, MdLockOutline } from "react-icons/md";
+import Button from "../../components/Button";
+import logo from "../../assets/logo.png";
+import Input from "../../components/Input";
+import * as S from "./styles";
+import Navbar from "../../components/Navbar/navbar";
+import Footer from "../../components/Footer/footer";
+import { AuthContext } from "../../context/auth";
 
 function LoginPage() {
-
   const { authenticated, login } = useContext(AuthContext);
 
   const [data, setData] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: "",
+  });
 
-
+  //validacao aqui pai
   function submit(e) {
-    e.preventDefault() //não faça a ação padrão de atualizar a pagina
+    e.preventDefault(); //não faça a ação padrão de atualizar a pagina
     const back = data;
-    console.log("back", data)
+    console.log("back", data);
     login(data.email, data.password); // integracao com contexto e api
   }
 
   function handle(e) {
-    const newdata = { ...data }
-    newdata[e.target.id] = e.target.value
-    setData(newdata)
-    console.log(newdata)
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+    console.log(newdata);
   }
 
   return (
@@ -41,12 +40,36 @@ function LoginPage() {
             <img src={logo} alt="logo" />
           </header>
 
-          <form onSubmit={e => submit(e)}>
+          <form onSubmit={(e) => submit(e)}>
             <p>LOGIN</p>
-            <Input onChange={e => handle(e)} id="email" placeholder="EMAIL" type="email" required />
-            <Input onChange={e => handle(e)} id="password" value={data.password} placeholder="SENHA" type="password" required />
+            <div className="email">
+              <div id="iconEmail">
+                <MdOutlineMail />
+              </div>
+              <Input
+                onChange={(e) => handle(e)}
+                id="email"
+                placeholder="EMAIL"
+                type="email"
+                required
+              />
+            </div>
+
+            <div className="password">
+              <div id="iconPassword">
+                <MdLockOutline />
+              </div>
+              <Input
+                onChange={(e) => handle(e)}
+                id="password"
+                value={data.password}
+                placeholder="SENHA"
+                type="password"
+                required
+              />
+            </div>
             <br></br>
-            <Button type="submit" title={'LOGIN'} required />
+            <Button type="submit" title={"LOGIN"} required />
           </form>
 
           <S.RegistrationButton href="/userRegistrationPage">
@@ -56,7 +79,7 @@ function LoginPage() {
       </S.Body>
       <Footer />
     </S.Container>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
