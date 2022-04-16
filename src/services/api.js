661905552a365518215ec772f0db0251de3c715c
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-04-16 04:06:21 -0300

    added api validation

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,58 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000",
 });
 
 export const createSession = async (email, password) => {
-    return api.post("/api/token/obtain/", { email, password });
-}
+  api
+    .post("/api/token/obtain/", { email, password })
+    .then((response) => {
+      console.log("response", response);
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Usuário ou senha inválidos");
+      return error;
+    });
+
+  // return api.post("/api/token/obtain/", { email, password });
+};
 
 export const userRegistration = async (name, email, cpf, phone, password) => {
-    return api.post("/user/", { name, email, cpf, phone, password });
-}
+  api
+    .post("/user/", { name, email, cpf, phone, password })
+    .then((response) => {
+      console.log("response", response);
+      alert("Usuário cadastrado com sucesso");
+      return response;
+    })
+    .catch((error) => {
+      console.log("error", error);
+      alert("Cadastro inválido");
+      return error;
+    });
+};
 
-export const collaboratorRegistration = async (name,
-    email, cpf, phone, password, occupation) => {
-    return api.post("/provider/", { name, email, cpf, phone, password, occupation});
-}
+export const collaboratorRegistration = async (
+  name,
+  email,
+  cpf,
+  phone,
+  password,
+  occupation
+) => {
+  api
+    .post("/provider/", { name, email, cpf, phone, password, occupation })
+    .then((response) => {
+      console.log("response", response);
+      alert("Usuário cadastrado com sucesso");
+      return response;
+    })
+    .catch((error) => {
+      console.log("error", error);
+      alert("Cadastro inválido");
+      return error;
+    });
+};
