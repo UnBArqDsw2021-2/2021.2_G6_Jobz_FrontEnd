b1f25a3def6630733140d38b7bf00d647b466fff
Rodrigo Balbino <rodrigo080599@hotmail.com>
2022-04-17 20:43:29 -0300

    Adicionando config inicial da conexão da pesquisa com o serviço
    Co-authored-by: Peh099 <pedrohenriquec099@gmail.com>

--- src/views/contatoPrestador/index.js
+++ src/views/contatoPrestador/index.js
@@ -18,8 +18,8 @@ function CollaboratorContactPage() {
     async function contactProvider() {
       const request = await api.get("/providerPresentation/")
         .then(request =>
-          setContactInfo(request.data[1],
-            setCpf(request.data[1].provider)))
+          setContactInfo(request.data[0],
+            setCpf(request.data[0].provider)))
         .catch(error => console.log(error))
 
       return request;
@@ -56,7 +56,7 @@ function CollaboratorContactPage() {
   return (
     <S.Container>
       <Navbar />
-      <h1>{provider.name}</h1>
+      <h1>{provider? provider.name: "Lorem ipsum"}</h1>
 
       <S.body>
         <S.LeftSide>
@@ -64,7 +64,7 @@ function CollaboratorContactPage() {
             <h3>Informações sobre o serviço</h3>
 
             <div className="description">
-              {contactInfo ? contactInfo.description : "DESCRIPTION"}
+              {contactInfo ? contactInfo.description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod vehicula porta. Vestibulum vitae condimentum ipsum. Fusce pellentesque odio metus, non viverra est efficitur eget. Donec finibus lobortis lobortis. Sed interdum nec dui ut commodo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean cursus, lorem et dignissim finibus, lorem leo faucibus ligula, vitae aliquam dolor risus et velit. Vivamus et massa fringilla, auctor nulla sit amet, tempor nisi. Nam egestas fringilla sem id sagittis. Integer porta sed tellus sodales porta. Nullam in nisi finibus, sagittis nunc eget, efficitur sem.Cras auctor, nibh et feugiat accumsan, arcu lorem laoreet mi, in viverra sapien nibh in eros.Duis imperdiet malesuada nulla, at pretium tellus condimentum non.Integer bibendum condimentum erat, quis mollis ex scelerisque sit amet.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Cras sed rhoncus mi.Sed viverra eget metus eu efficitur.Interdum et malesuada fames ac ante ipsum primis in faucibus.Cras venenatis euismod ante.Morbi dapibus bibendum lacus, nec euismod elit consectetur ac.Suspendisse id finibus augue.Fusce tincidunt ligula eu ligula suscipit, in suscipit dui consequat.Donec varius lectus at libero rhoncus, at feugiat sapien efficitur."}
 
             </div>
           </S.description>
--- src/views/explore/explorePage/index.js
+++ src/views/explore/explorePage/index.js
@@ -10,12 +10,44 @@ import Encanador from '../../../assets/EncadorExample.png'
 import Pedreiro from '../../../assets/Pedreiro.png'
 import Button from '../../../components/Button/index'
 import { api } from '../../../services/api'
+import { useNavigate } from 'react-router-dom'
 
 function ExplorePage() {
-  const content =  [
+
+  const [user, setUser] = useState();
+  let navigate = useNavigate()
+  let i = 0;
+
+  useEffect(() => {
+    api
+      .get("/provider")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+
+  const listItems = user?.map((number) =>
+    <div >
+      <button id="1" onClick={navigateToPage} className="imagesButton"> 
+      <img src={image(user[i]?.occupation)} alt="Imagem da ocupacao"></img>
+      </button>
+      <p id="titleService"> Prestador: {user[i]?.name}</p>
+      <p id="descriptionService">Serviço: {ocuppation(user[i++]?.occupation)}</p>
+    </div>
+
+  );
+
+  function navigateToPage() {
+    navigate("/collaboratorContactPage")
+  }
+
+
+  const content = [
     {
-        value: 1,
-        label: <Button
+      value: 1,
+      label: <Button
         path="/exploreDayLabore"
         title="Diarista"
         ButtonColor="transparent"
@@ -28,8 +60,8 @@ function ExplorePage() {
       />
     },
     {
-        value: 2,
-        label: <Button
+      value: 2,
+      label: <Button
         path="/explorePlumber"
         title="Encanador"
         ButtonColor="transparent"
@@ -42,8 +74,8 @@ function ExplorePage() {
       />
     },
     {
-        value: 3,
-        label: <Button
+      value: 3,
+      label: <Button
         path="/exploreBricklayer"
         title="Pedreiro"
         ButtonColor="transparent"
@@ -56,8 +88,8 @@ function ExplorePage() {
       />
     },
     {
-        value: 4,
-        label: <Button
+      value: 4,
+      label: <Button
         path="/exploreTI"
         title="Tecnico"
         ButtonColor="transparent"
@@ -69,85 +101,67 @@ function ExplorePage() {
         boxShadow="0px"
       />
     }
-]
+  ]
 
-  function ocuppation(e){
-    if (e===1){
+  function ocuppation(e) {
+    if (e === 1) {
       return "Diarista"
     }
-    else if(e===2){
+    else if (e === 2) {
       return "Encanador"
     }
-    else if(e===3){
+    else if (e === 3) {
       return "Pedreiro"
     }
-    else if (e===4){
+    else if (e === 4) {
       return "Tecnico"
     }
   }
 
-  function image(e){
-    if (e===1){
+  function image(e) {
+    if (e === 1) {
       return Diarista
     }
-    else if(e===2){
+    else if (e === 2) {
       return Encanador
     }
-    else if(e===3){
+    else if (e === 3) {
       return Pedreiro
     }
-    else if (e===4){
+    else if (e === 4) {
       return serviceTI
     }
   }
 
-  const [user, setUser] = useState();
 
-  useEffect(() => {
-    api
-      .get("/provider")
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-  let i=0;
 
-  const listItems = user?.map((number) =>
-        <div>
-        <button className="imagesButton"><img src={image(user[i]?.occupation)} alt="Imagem da ocupacao"></img></button>        
-        <p id="titleService"> Prestador: {user[i]?.name}</p>
-        <p id="descriptionService">Serviço: {ocuppation(user[i++]?.occupation)}</p>
-        </div>
-    );
-
-//<p>Usuário: {user[0]?.name}</p>
+  //<p>Usuário: {user[0]?.name}</p>
 
   return (
     <S.Container>
       <Navbar />
       <S.Body>
         <div id="GroupInformation">
-            <header>
-                <div id="link">
-                    <a href="/">Home</a>
-                    <img src={sinal} alt="sinal maior"></img>
-                    <p id="service">Serviços de TI</p>
-                </div>
-                <p id="title">Serviços em geral</p>
-                <p id="description">Aqui você encontra todos os serviços cadastrados por nossos prestadores.</p>
-                <div id="order-by">
-                    <p id="order">Ordernar por:</p>
-                    <Select id="occupation" placeholder="Selecione a Ocupação" options={content} required />
-                </div>
-                <div id="serviceList">
-                    <li>
-                        {listItems}
-                    </li>
-                </div>
-            </header>
+          <header>
+            <div id="link">
+              <a href="/">Home</a>
+              <img src={sinal} alt="sinal maior"></img>
+              <p id="service">Serviços de TI</p>
+            </div>
+            <p id="title">Serviços em geral</p>
+            <p id="description">Aqui você encontra todos os serviços cadastrados por nossos prestadores.</p>
+            <div id="order-by">
+              <p id="order">Ordernar por:</p>
+              <Select id="occupation" placeholder="Selecione a Ocupação" options={content} required />
+            </div>
+            <div id="serviceList">
+              <li key="{listItems}">
+                {listItems}
+              </li>
+            </div>
+          </header>
         </div>
-      </S.Body>     
+      </S.Body>
       <Footer />
     </S.Container>
   )
